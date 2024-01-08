5ed42691f3ffce7f7605a4bd1f0d474fe07b88ef
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2024-01-08 20:13:37 +0100

    start using remote data base - MongoDB Atlas

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
-mongoose.connect('mongodb://0.0.0.0:27017/newWaveDB', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(
+	'mongodb+srv://dariuszwilk1993:fZX6zTbJjmw9qZYD@cluster0.nazijyh.mongodb.net/newWaveDB?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	}
+);
 const db = mongoose.connection;
 
 db.once('open', () => {
